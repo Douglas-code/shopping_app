30d0882df3b1e730fc33e3b4236f7c479978e0dd
Douglas Rodrigues Pereira <douglasrodrigues20161@outlook.com>
2021-03-25 21:20:58 -0300

    Adicionando SliverAppBar

--- lib/pages/product.page.dart
+++ lib/pages/product.page.dart
@@ -4,12 +4,28 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.blue,
-        child: Center(
-          child: Text("Detalhes"),
-        ),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.white.withOpacity(0),
+              elevation: 0.0,
+              expandedHeight: 500.00,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                background: Image.asset(
+                  "assets/product-10.png",
+                  width: 160,
+                  height: 160,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          ];
+        },
+        body: Container(),
       ),
     );
   }
